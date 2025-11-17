aaaf5edcf483bdee772705bb9f3ef0df7687e631
Joshua-C-Bryant <josh.c.bryant1@gmail.com>
2025-11-17 11:35:34 -0600

    Working with lists & Rock Paper Scissors Game

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,8 @@
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+import random
+
+random_friend_num = random.randint(0,4)
+
+print(friends[random_friend_num])
+
+print(random.choice(friends))
