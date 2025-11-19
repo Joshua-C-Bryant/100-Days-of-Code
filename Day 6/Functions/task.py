67161f6d926d6e33e6cf990a9f998cef6ae6ad37
Joshua-C-Bryant <josh.c.bryant1@gmail.com>
2025-11-19 15:59:14 -0600

    Functions

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,8 @@
+#Creating the function
+def get_user_name():
+    name = input("What is your name? ")
+    print("Hello, " + name)
+    # Inside the function
+
+#Outside the function
+get_user_name() # Calling the function
