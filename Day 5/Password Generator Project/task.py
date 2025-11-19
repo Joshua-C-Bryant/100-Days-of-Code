546a754bbb21b0edf0ed605eea8f26bdf571ddf8
Joshua-C-Bryant <josh.c.bryant1@gmail.com>
2025-11-19 15:56:46 -0600

    Password Generator

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,3 +1,5 @@
+from operator import concat
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,4 +9,17 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+import random
+
+pass_letters = random.sample(letters, k=nr_letters)
+pass_symbols = random.sample(symbols, k=nr_symbols)
+pass_numbers = random.sample(numbers, k=nr_numbers)
+
+combined = pass_letters + pass_symbols + pass_numbers
+
+random.shuffle(combined)
+
+generated_password = "".join(combined)
+
+print(generated_password)
 
