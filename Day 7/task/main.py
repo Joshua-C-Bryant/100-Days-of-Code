f176b52ce95487ca97dfa0595d42041ae374ac28
Joshua-C-Bryant <josh.c.bryant1@gmail.com>
2025-11-21 11:11:47 -0600

    Hangman Game

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,34 +1,111 @@
 import random
 
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["aardvark", "baboon", "camel"]
+
+# TODO-1
+lives = 6
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-# Create initial display with blanks
-display = ""
-for _ in chosen_word:
-    display += "_"
+# Initial display
+display = "_" * len(chosen_word)
 print(display)
 
-# TODO-1: Use a while loop to let the user guess again.
-while "_" in display:
+game_over = False
+correct_letters = []
+
+while not game_over:
     guess = input("Guess a letter: ").lower()
 
-    # TODO-2: Keep previous correct letters.
+    # Rebuild the display every round
     new_display = ""
     for index in range(len(chosen_word)):
         letter = chosen_word[index]
 
         if letter == guess:
-            # Correct guess, reveal the letter
+            new_display += letter
+            if guess not in correct_letters:
+                correct_letters.append(guess)
+        elif letter in correct_letters:
             new_display += letter
         else:
-            # Keep the previous letter or underscore
-            new_display += display[index]
+            new_display += "_"
 
     display = new_display
     print(display)
 
-print("You guessed the word!")
+    # TODO-2: Lose a life if wrong
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"Wrong! You lose a life. Lives left: {lives}")
+        if lives == 0:
+            print("Game Over")
+            game_over = True
+
+    # Win check
+    if "_" not in display:
+        print("You win!")
+        game_over = True
+
+    # TODO-3: Show hangman stage
+    print(stages[lives])
 
