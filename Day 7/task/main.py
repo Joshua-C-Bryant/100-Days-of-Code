d9c00cca8cbb6202d820b5446b01292c2a9870d2
Joshua-C-Bryant <josh.c.bryant1@gmail.com>
2025-11-21 10:59:36 -0600

    Hangman Game

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,27 +1,34 @@
 import random
+
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-placeholder = ""
-word_length = len(chosen_word)
-for position in range(word_length):
-    placeholder += "_"
-print(placeholder)
-
-# TODO-1: - Use a while loop to let the user guess again.
-
-guess = input("Guess a letter: ").lower()
-
+# Create initial display with blanks
 display = ""
-
-# TODO-2: Change the for loop so that you keep the previous correct letters in display.
-
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
-
+for _ in chosen_word:
+    display += "_"
 print(display)
+
+# TODO-1: Use a while loop to let the user guess again.
+while "_" in display:
+    guess = input("Guess a letter: ").lower()
+
+    # TODO-2: Keep previous correct letters.
+    new_display = ""
+    for index in range(len(chosen_word)):
+        letter = chosen_word[index]
+
+        if letter == guess:
+            # Correct guess, reveal the letter
+            new_display += letter
+        else:
+            # Keep the previous letter or underscore
+            new_display += display[index]
+
+    display = new_display
+    print(display)
+
+print("You guessed the word!")
+
